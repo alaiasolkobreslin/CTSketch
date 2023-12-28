30c03fcc56ebc39f0cb03ffbb117a50c9ad09959
Seewon Choi <seewon@seas.upenn.edu>
2023-12-28 11:14:27 -0500

    leaf classification

--- demo/task_program.py
+++ demo/task_program.py
@@ -6,3 +6,31 @@ def sum_3(xa, xb, xc):
 
 def sum_4(xa, xb, xc, xd):
   return xa + xb + xc + xd
+
+def classify_plantvillage(margin, shape, apex):
+  if margin == 'entire':
+    if shape == 'cordate':
+      return "potato"
+    else:
+      if apex == 'aristate':
+        return "pepper"
+      elif apex == 'round':
+        return "soybean"
+      else:
+        return "blueberry"
+  elif margin == 'serrate':
+    if shape == 'cordate': 
+      return "raspberry"
+    elif shape == 'heart':
+      return 'grape'
+    else: 
+      if apex == 'half':
+        return "cherry"
+      else:
+        return 'apple'
+  elif margin == 'lobbed':
+    return "tomato"
+  elif margin == 'mixed':
+    return 'strawberry'
+  else:
+    return "peach"
