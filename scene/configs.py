7bd2396bbabc98b82ccd656ac27f2371f3d84c69
Seewon Choi <seewon@seas.upenn.edu>
2024-07-30 14:42:25 -0400

    remove old call_llm scene code

--- scene/configs.py
+++ scene/configs.py
@@ -10,7 +10,7 @@ client = OpenAI(
 system_msg = "You are an expert at identifying room types based on the object detected. Give short single responses."
 question = "\n What type of room is most likely? Choose among basement, bathroom, bedroom, living room, home lobby, office, lab, kitchen, dining room."
 queries = {}
-with open('scene/llm_single.pkl', 'rb') as f: 
+with open('scene/llm.pkl', 'rb') as f: 
   queries = pickle.load(f)
 
 def classify_compose(objects):
@@ -22,49 +22,11 @@ def classify_cot(objects):
   return answer
 
 def classify_llm(objects):
-  random_scene = ['basement', 'bathroom', 'bedroom', 'dining', 'kitchen', 'lab', 'living', 'lobby', 'office']
-  random.shuffle(random_scene)
-  answer = call_llm(objects)
-  counts = torch.zeros(9)
-  for a in answer:
-    s = parse_response(a)
-    counts[random_scene.index(s)] += 1
-  return random_scene[counts.argmax()]
-
-def call_llm(objects):
-  r = []
-  for o in objects:
-    if o == 'skip' or o == 'ball': 
-      continue
-    prompt = f"There is a {o}."
-    if o in queries.keys():
-      r.append(queries[o])
-      continue
-    response = client.chat.completions.create(
-                model="gpt-4o",
-                messages=[
-                  {"role": "system", "content": system_msg},
-                  {"role": "user", "content": prompt + question}
-                ],
-                top_p=1e-8
-              )
-    if response.choices[0].finish_reason == 'stop':
-      ans = response.choices[0].message.content.lower()
-      print(ans)
-      queries[o] = ans
-      with open('scene/llm_single.pkl', 'wb') as f:
-        pickle.dump(queries, f)
-      r.append(ans)
-    else: 
-      raise Exception("LLM failed to provide an answer") 
-  return r
-
-def classify_llm_all(objects):
   answer = call_llm(objects)
   answer = parse_response(answer)
   return answer
 
-def call_llm_all(objects):
+def call_llm(objects):
   objects.sort()
   objects = list(set(objects))
   if 'skip' in objects: objects.remove('skip')
