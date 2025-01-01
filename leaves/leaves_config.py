438f682b9f38d8c04a89d87e6e6a67803839a12d
Seewon Choi <seewon@seas.upenn.edu>
2025-01-01 11:46:32 -0500

    leaves wandb

---
+++ leaves/leaves_config.py
@@ -0,0 +1,209 @@
+import os
+from typing import *
+import json
+import random
+import pickle
+
+import torch
+import torchvision
+from torch import nn
+from PIL import Image
+
+#client = OpenAI(
+#  api_key='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'
+#)
+
+leaves_img_transform = torchvision.transforms.Compose([
+  torchvision.transforms.ToTensor(),
+  torchvision.transforms.Normalize(
+    (0.1307,), (0.3081,)
+  )
+])
+
+class LeavesDataset(torch.utils.data.Dataset):
+  """
+  :param data_root, the root directory of the data folder
+  """
+  def __init__(
+    self,
+    data_root: str,
+    data_dir: str,
+    n_train: int,
+    transform: Optional[Callable] = leaves_img_transform,
+  ):
+    self.transform = transform
+    if data_dir == 'leaf_11': self.labels = l11_labels
+    else: self.labels = []
+    
+    # Get all image paths and their labels
+    self.samples = []
+    data_dir = os.path.join(data_root, data_dir)
+    data_dirs = os.listdir(data_dir)
+    for sample_group in data_dirs:
+      sample_group_dir = os.path.join(data_dir, sample_group)
+      if not os.path.isdir(sample_group_dir) or not sample_group in self.labels:
+        continue
+      label = self.labels.index(sample_group)
+      sample_group_files = os.listdir(sample_group_dir)
+      for idx in random.sample(range(len(sample_group_files)), min(n_train, len(sample_group_files))):
+        sample_img_path = os.path.join(sample_group_dir, sample_group_files[idx])
+        if sample_img_path.endswith('JPG') or sample_img_path.endswith('png'):
+          self.samples.append((sample_img_path, label))
+    
+    self.index_map = list(range(len(self.samples)))
+    random.shuffle(self.index_map)
+
+  def __len__(self):
+    return len(self.samples)
+
+  def __getitem__(self, idx):
+    (img_path, label) = self.samples[self.index_map[idx]]
+    img = Image.open(open(img_path, "rb"))
+    img = self.transform(img)
+    return (img, label)
+  
+  @staticmethod
+  def collate_fn(batch):
+    imgs = torch.stack([item[0] for item in batch])
+    labels = torch.stack([torch.tensor(item[1]).long() for item in batch])
+    return (imgs, labels)
+
+def leaves_loader(data_root, data_dir, batch_size, n_train, n_test, n_val):
+  dataset = LeavesDataset(data_root, data_dir, (n_train+n_test+n_val))
+  num_train = n_train*11
+  num_val = n_val*11
+  num_test = len(dataset) - num_train - num_val
+  (train_dataset, val_dataset, test_dataset) = torch.utils.data.random_split(dataset, [num_train, num_val, num_test])
+  train_loader = torch.utils.data.DataLoader(train_dataset, collate_fn=LeavesDataset.collate_fn, batch_size=batch_size, shuffle=True)
+  val_loader = torch.utils.data.DataLoader(val_dataset, collate_fn=LeavesDataset.collate_fn, batch_size=batch_size, shuffle=True)
+  test_loader = torch.utils.data.DataLoader(test_dataset, collate_fn=LeavesDataset.collate_fn, batch_size=batch_size, shuffle=True)
+  return (train_loader, val_loader, test_loader)
+
+class LeafNet(nn.Module):
+  def __init__(self, num_features):
+    super(LeafNet, self).__init__()
+    self.num_features = num_features
+    self.dim = l11_dim
+
+    # CNN
+    self.cnn = nn.Sequential(
+      nn.Conv2d(3, 32, 10, 1),
+      nn.ReLU(),
+      nn.MaxPool2d(3),
+      nn.Conv2d(32, 64, 5, 1),
+      nn.ReLU(),
+      nn.MaxPool2d(3),
+      nn.Conv2d(64, 128, 3, 1),
+      nn.ReLU(),
+      nn.MaxPool2d(2),
+      nn.Conv2d(128, 128, 3, 1),
+      nn.ReLU(),
+      nn.MaxPool2d(2),
+      nn.Flatten(),
+    )
+
+    # Fully connected for 'features'
+    self.features_fc = nn.Sequential(
+      nn.Linear(self.dim, self.dim),
+      nn.ReLU(),
+      nn.Linear(self.dim, self.num_features),
+      nn.Softmax(dim=1)
+    )
+  
+  def forward(self, x):
+    x = self.cnn(x)
+    x = self.features_fc(x)   
+    return x
+
+def classify_11(margin, shape, texture):
+  if margin == 'serrate': return 'Ocimum basilicum'
+  elif margin == 'indented': return 'Jatropha curcas'
+  elif margin == 'lobed': return 'Platanus orientalis'
+  elif margin == 'serrulate': return "Citrus limon"
+  elif margin == 'entire':
+    if shape == 'ovate': return 'Pongamia Pinnata'
+    elif shape == 'lanceolate': return 'Mangifera indica'
+    elif shape == 'oblong': return 'Syzygium cumini'
+    elif shape == 'obovate': return "Psidium guajava"
+    else:
+      if texture == 'leathery': return "Alstonia Scholaris"
+      elif texture == 'rough': return "Terminalia Arjuna"
+      elif texture == 'glossy': return "Citrus limon"
+      else: return "Punica granatum"
+  else:
+    if shape == 'elliptical': return 'Terminalia Arjuna'
+    elif shape == 'lanceolate': return "Mangifera indica"
+    else: return 'Syzygium cumini'
+
+l11_margin = ['entire', 'indented', 'lobed', 'serrate', 'serrulate', 'undulate']
+l11_shape = ['elliptical', 'lanceolate', 'oblong', 'obovate', 'ovate']
+l11_texture = ['glossy', 'leathery', 'medium', 'rough']
+l11_labels = ['Alstonia Scholaris', 'Citrus limon', 'Jatropha curcas', 'Mangifera indica', 'Ocimum basilicum',
+              'Platanus orientalis', 'Pongamia Pinnata', 'Psidium guajava', 'Punica granatum', 'Syzygium cumini', 'Terminalia Arjuna']
+l11_dim = 2304
+
+l11_4_system = "You are an expert in classifying plant species based on the margin, shape, and texture of the leaves. You are designed to output a single JSON."
+l11_4_one = ['entire', 'lobed', 'serrate']
+l11_4_two = ['cordate', 'lanceolate', 'oblong', 'oval', 'ovate', 'palmate']
+l11_4_three = ['glossy', 'papery', 'smooth']
+
+queries = {}
+
+with open('leaves/leaf11.pkl', 'rb') as f: 
+  queries = pickle.load(f)
+
+labels = l11_labels
+features_1 = l11_4_one
+features_2 = l11_4_two
+features_3 = l11_4_three
+system_msg = l11_4_system
+
+def classify_llm(feature1, feature2, feature3):
+  result1 = call_llm(labels, features_1)
+  plants1 = parse_response(result1, feature1)
+  if len(plants1) == 1: return plants1[0]
+  elif len(plants1) == 0: 
+    plants1 = labels # return 'unknown'
+  else:
+    results2 = call_llm(plants1, features_2)
+    plants2 = parse_response(results2, feature2)
+    if len(plants2) == 1: return plants2[0]
+    elif len(plants2) == 0: 
+      plants2 = plants1  # return 'unknown'
+    results3 = call_llm(plants2, features_3)
+    plants3 = parse_response(results3, feature3)
+    if len(plants3) == 1: return plants3[0]
+    elif len(plants3) == 0: return plants2[random.randrange(len(plants2))] # return 'unknown'
+    else: return plants3[random.randrange(len(plants3))]
+
+def call_llm(plants, features):
+  user_list = "* " + "\n* ".join(plants)
+  question = "\n\nClassify each into one of: " + ", ".join(features) + "."
+  format = "\n\nGive your answer without explanation."
+  user_msg = user_list + question
+  if user_msg in queries.keys():
+    return queries[user_msg]
+  else: raise Exception("GPT")
+  response = client.chat.completions.create(
+              model="gpt-4-1106-preview",
+              messages=[
+                {"role": "system", "content": system_msg},
+                {"role": "user", "content": user_msg + format}
+              ],
+              top_p=0.00000001
+            )
+  if response.choices[0].finish_reason == 'stop':
+    ans = response.choices[0].message.content
+    print(ans[7:-3])
+    queries[user_msg] = ans[7:-3] # ans
+    with open('leaves/leaf11.pkl', 'wb') as f: 
+      pickle.dump(queries, f)
+    return ans[7:-3]
+  raise Exception("LLM failed to provide an answer") 
+
+def parse_response(result, target):
+  dict = json.loads(result)
+  plants = []
+  for plant in dict.keys():
+    if dict[plant] == target: plants.append(plant)
+  return plants
