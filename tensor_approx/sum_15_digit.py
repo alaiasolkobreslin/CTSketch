ab9e69b4762138e1017d6d5228f54bdcbc82ab0b
Alaia Solko-Breslin <ajs644@cornell.edu>
2025-01-02 11:47:05 -0500

    better sum15

--- tensor_approx/sum_15_digit.py
+++ tensor_approx/sum_15_digit.py
@@ -5,6 +5,8 @@ from tqdm import tqdm
 from functools import reduce
 from argparse import ArgumentParser
 from sklearn.metrics import confusion_matrix
+from time import time
+import wandb
 
 import torch
 import torch.nn.functional as F
@@ -14,15 +16,16 @@ from mnist_config import MNISTSumNet, mnist_multi_digit_sum2_loader
 import sketch_config
 from tensor_sketch import TensorSketch
 
-# add two numbers with number of digits=digits
-def full_thetas(digit, samples):
+def pretrain(digit, samples):
+    t0 = time()
     # first calculate theta for the two-digit addition
     full_theta1 = sketch_config.full_theta2(digit=digit, elems=10, output_dim=19, samples=samples)
     # then calculate theta for the carry addition
     theta_i_0 = torch.arange(0, 19).unsqueeze(1).repeat(1, 10)
     theta_i_carry = torch.arange(1, 20).unsqueeze(1).repeat(1, 10)
     full_theta2 = torch.cat((theta_i_0, theta_i_carry), dim=1)
-        
+    t1 = time()
+    wandb.log({"pretrain": t1 - t0})
     return full_theta1, full_theta2
 
 
@@ -40,7 +43,7 @@ class Trainer():
     self.rerr = [1] * (digits*2)
     self.t1 = None
     self.t2 = None
-    self.full_theta1, self.full_theta2 = full_thetas(digits, 0)
+    self.full_theta1, self.full_theta2 = pretrain(digits, 0)
 
   def sub_program(self, n, d, *base_inputs):
     t = self.t1.long().to(device)
@@ -109,25 +112,26 @@ class Trainer():
     self.network.train()
     num_items = 0
     total_correct = 0
+    train_loss = 0
+    
     pred_powers_of_10 = torch.arange(self.digits + 1, device=device).unsqueeze(0)
     target_powers_of_10 = torch.arange(self.digits + 1).unsqueeze(0)
-    iter = tqdm(self.train_loader, total=len(self.train_loader))
-    for (data, target, _) in iter:
+    for (i, (data, target, _)) in enumerate(train_loader):
       self.optimizer.zero_grad()
       output_t = self.network(tuple([data_i.to(device) for data_i in data]))
       rerr, output = self.program(*tuple(output_t))
-
       loss = self.loss(output, target.to(device))
       loss.backward()
       self.optimizer.step()
-      
       final_pred = torch.sum(output.argmax(dim=-1) * (10 ** pred_powers_of_10), dim=1)
       final_target = torch.sum(target * (10 ** target_powers_of_10), dim=1)
       total_correct += (final_pred.to('cpu') == final_target).sum()
-      
+      train_loss += loss.item()
+      avg_loss = train_loss / (i + 1)
       num_items += output.shape[0]
       correct_perc = 100. * total_correct / num_items
-      iter.set_description(f"[Train {epoch}] Err:{rerr:.4f} Loss: {loss.item():.4f} Accuracy: {correct_perc:.4f}%")
+    print(f"[Train Epoch {epoch}] Loss: {avg_loss:.4f}, Overall Accuracy: {int(total_correct)}/{int(num_items)} {correct_perc:.2f}%")
+    return train_loss
 
   def test_epoch(self, epoch):
     self.network.eval()
@@ -139,8 +143,7 @@ class Trainer():
     pred_powers_of_10 = torch.arange(self.digits - 1, -1 , -1, device=device).unsqueeze(0)
     target_powers_of_10 = torch.arange(0, self.digits + 1).unsqueeze(0)
     with torch.no_grad():
-      iter = tqdm(self.test_loader, total=len(self.test_loader))
-      for (data, target, digits) in iter:
+      for (data, target, digits) in test_loader:
         output_t = self.network(tuple([data_i.to(device) for data_i in data]))
         output = torch.stack(output_t, dim=0).argmax(dim=-1)
         # split output into two tensors
@@ -156,18 +159,29 @@ class Trainer():
         digit_correct += (output.t().flatten() == digits.flatten().to(device)).sum()
         perc = 100. * correct / num_items
         perc2 = 100. * digit_correct / (num_items * self.digits * 2)
-        iter.set_description(f"[Test Epoch {epoch}] Accuracy: {correct}/{num_items} ({perc:.2f}%) Digit Accuracy ({perc2:.2f}%)")
 
-      cf_matrix = confusion_matrix(digits_gt, digits_pred)
-      print(cf_matrix)
+    print(f"[Test Epoch {epoch}] Total correct {int(correct)}/{int(num_items)} ({perc:.2f}) Digit correct {perc2:.2f}%")
     
     if self.save_model:
       torch.save(self.network.state_dict(), self.model_dir+f"/best.pth")
+    
+    return perc, perc2
 
   def train(self, n_epochs):
     for epoch in range(1, n_epochs + 1):
-      self.train_epoch(epoch)
-      self.test_epoch(epoch)
+      t0 = time()
+      train_loss = self.train_epoch(epoch)
+      t1 = time()
+      test_acc, test_digit_acc = self.test_epoch(epoch)
+      t2 = time()
+      
+      wandb.log({
+        "train_loss": train_loss,
+        "test_acc": test_acc,
+        "test_digit_acc": test_digit_acc,
+        "train time": t1 - t0,
+        "test time": t2 - t1,
+        "epoch time": t2 - t0})
       
       if self.save_model: 
         torch.save(self.network.state_dict(), self.model_dir+f"/latest.pth")
@@ -175,13 +189,13 @@ class Trainer():
 if __name__ == "__main__":
   # Argument parser
   parser = ArgumentParser("mnist_sum")
-  parser.add_argument("--n-epochs", type=int, default=200)
+  parser.add_argument("--n-epochs", type=int, default=20)
   parser.add_argument("--batch-size", type=int, default=64)
   parser.add_argument("--learning-rate", type=float, default=1e-3)
   parser.add_argument("--method", type=str, default='tt')
   parser.add_argument("--seed", type=int, default=1234)
   parser.add_argument("--jit", action="store_true")
-  parser.add_argument("--digits", type=int, default=4)
+  parser.add_argument("--digits", type=int, default=15)
   parser.add_argument("--dispatch", type=str, default="parallel")
   args = parser.parse_args()
 
@@ -191,22 +205,33 @@ if __name__ == "__main__":
   learning_rate = args.learning_rate
   digit = args.digits
   method = args.method
-  digit = 15
+  
+  for seed in [4321]:
 
-  torch.manual_seed(args.seed)
-  random.seed(args.seed)
+    torch.manual_seed(seed)
+    random.seed(seed)
 
-  if torch.cuda.is_available(): device = torch.device('cuda')
-  else: device = torch.device('cpu')
+    if torch.cuda.is_available(): device = torch.device('cuda')
+    else: device = torch.device('cpu')
 
-  # Data
-  data_dir = os.path.abspath(os.path.join(os.path.abspath(__file__), "../data"))
-  model_dir = os.path.abspath(os.path.join(os.path.abspath(__file__), f"../model/sum_{digit}"))
-  os.makedirs(model_dir, exist_ok=True)
+    # Data
+    data_dir = os.path.abspath(os.path.join(os.path.abspath(__file__), "../data"))
+    model_dir = os.path.abspath(os.path.join(os.path.abspath(__file__), f"../model/sum_{digit}"))
+    os.makedirs(model_dir, exist_ok=True)
 
-  # Dataloaders
-  train_loader, test_loader = mnist_multi_digit_sum2_loader(data_dir, batch_size, digit)
-
-  # Create trainer and train
-  trainer = Trainer(MNISTSumNet, method, digit, train_loader, test_loader, model_dir, learning_rate)
-  trainer.train(n_epochs)
+    # Dataloaders
+    train_loader, test_loader = mnist_multi_digit_sum2_loader(data_dir, batch_size, digit)
+    
+    # Setup wandb
+    config = vars(args)
+    run = wandb.init(
+      project=f'add-two-{digit}-digit',
+      name = f'{seed}',
+      config=config
+    )
+    
+    # Create and run trainer
+    trainer = Trainer(MNISTSumNet, method, digit, train_loader, test_loader, model_dir, learning_rate)
+    trainer.train(n_epochs)
+    
+    run.finish()
