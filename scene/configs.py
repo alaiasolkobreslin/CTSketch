95f0acefafb524b6cebd54fb7c471207f8fbfafa
Alaia Solko-Breslin <ajs644@cornell.edu>
2025-01-29 23:19:56 -0500

    committing old work

--- scene/configs.py
+++ scene/configs.py
@@ -2,6 +2,37 @@ from openai import OpenAI
 import random
 import torch
 import pickle
+import dataset
+
+from collections import Counter
+
+objects = dataset.objects
+
+scene_objects = {
+    'lobby': dataset.lobby_objects,
+    'lab': dataset.lab_objects,
+    'bathroom': dataset.bathroom_objects,
+    'bedroom': dataset.bedroom_objects,
+    'living': dataset.living_objects,
+    'kitchen': dataset.kitchen_objects,
+    'dining': dataset.dining_objects,
+    'office': dataset.office_objects,
+    'basement': dataset.basement_objects
+}
+
+def find_most_likely_scene(objects):
+    # Create a counter for each scene
+    scene_counter = Counter()
+
+    # Count occurrences of objects in each scene
+    for obj in objects:
+        for scene, scene_objs in scene_objects.items():
+            if obj in scene_objs:
+                scene_counter[scene] += 1
+
+    # Find the scene with the highest count
+    most_likely_scene = scene_counter.most_common(1)
+    return most_likely_scene[0][0] if most_likely_scene else "bedroom"
 
 client = OpenAI(
   api_key='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'
@@ -10,11 +41,16 @@ client = OpenAI(
 system_msg = "You are an expert at identifying room types based on the object detected. Give short single responses."
 question = "\n What type of room is most likely? Choose among basement, bathroom, bedroom, living room, home lobby, office, lab, kitchen, dining room."
 queries = {}
-with open('scene/llm.pkl', 'rb') as f: 
+with open('/Users/alaiasolko-breslin/Penn/research/blackbox-learning/scene/scene/llm.pkl', 'rb') as f: 
   queries = pickle.load(f)
 
 def classify_compose(objects):
-  return
+  objects = list(set(objects))
+  scene = find_most_likely_scene(objects)
+  new_objects = scene_objects[scene][:5]
+  full_objects = objects + new_objects
+  return list(set(full_objects))
+  # return [scene]
 
 def classify_cot(objects):
   better_objects = classify_compose(objects)
@@ -22,11 +58,22 @@ def classify_cot(objects):
   return answer
 
 def classify_llm(objects):
-  answer = call_llm(objects)
-  answer = parse_response(answer)
-  return answer
+  objects = list(set(objects))
+  scene = find_most_likely_scene(objects)
+  return scene
+  # if 'wardrobe' in objects or 'bed' in objects: return "bedroom"
+  # elif 'machines' in objects or 'printer' in objects: return 'office'
+  # elif 'sofa' in objects or 'tv' in objects: return 'living'
+  # elif 'toilet' in objects or 'sink' in objects: return 'bathroom'
+  # elif 'storage objects' in objects or 'washer' in objects: return 'basement'
+  # elif 'sink' in objects or 'refrigerator' in objects or 'oven' in objects or 'stove' in objects: return 'kitchen'
+  # return "kitchen"
+  # answer = call_llm(objects)
+  # answer = parse_response(answer)
+  # return answer
 
 def call_llm(objects):
+  raise Exception("shouldn't be here")
   objects.sort()
   objects = list(set(objects))
   if 'skip' in objects: objects.remove('skip')
@@ -48,7 +95,7 @@ def call_llm(objects):
     ans = response.choices[0].message.content.lower()
     print(ans)
     queries[user_list] = ans
-    with open('scene/llm.pkl', 'wb') as f:
+    with open('/Users/alaiasolko-breslin/Penn/research/blackbox-learning/scene/scene/llm.pkl', 'wb') as f:
       pickle.dump(queries, f)
     return ans
   raise Exception("LLM failed to provide an answer") 
